134b8e6b3651f69e882668c8b812ca54ad110ca1
blackbot00 <109160013+blackbot00@users.noreply.github.com>
2026-02-24 07:55:53 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+
+class Config:
+    API_ID = 12345
+    API_HASH = "your_api_hash"
+    BOT_TOKEN = "your_bot_token"
+    MONGO_URI = "your_mongodb_url"
+    AUTH_CHANNEL = -100... # Channel ID
+    INDEX_CHANNEL = -100... # Files irukkura channel
+    LOG_CHANNEL = -100... # Log channel ID
+    
+    # Pricing in Stars (Approx calculation)
+    PRICE_1W = 20  # ~29 INR
+    PRICE_1M = 35  # ~49 INR
+    PRICE_3M = 55  # ~79 INR
